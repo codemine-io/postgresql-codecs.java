93721f9277b6d231f7f5e6da1bb6958c6dafff8a
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-03-30 15:09:21 +0300

    Add missing tests

---
+++ src/test/java/io/codemine/postgresql/codecs/Int4CodecTest.java
@@ -0,0 +1,7 @@
+package io.codemine.postgresql.codecs;
+
+public class Int4CodecTest extends CodecTestBase<Integer> {
+  public Int4CodecTest() {
+    super(Codec.INT4);
+  }
+}
---
+++ src/test/java/io/codemine/postgresql/codecs/TextCodecTest.java
@@ -0,0 +1,7 @@
+package io.codemine.postgresql.codecs;
+
+public class TextCodecTest extends CodecTestBase<String> {
+  public TextCodecTest() {
+    super(Codec.TEXT);
+  }
+}

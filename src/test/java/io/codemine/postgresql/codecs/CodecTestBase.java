c24672b4055a7a644d1b64501d823b925d5a6b45
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-03-30 14:06:22 +0300

    Add array unit-tests

--- src/test/java/io/codemine/postgresql/codecs/CodecTestBase.java
+++ src/test/java/io/codemine/postgresql/codecs/CodecTestBase.java
@@ -14,12 +14,14 @@ abstract class CodecTestBase<A> {
   private final Codec<A> codec;
   private final Class<A> type;
   private final Codec<List<A>> arrayCodec;
+  private final Codec<List<List<A>>> arrayArrayCodec;
 
   @SuppressWarnings("unchecked")
   protected CodecTestBase(Codec<A> codec, Class<A> type) {
     this.codec = codec;
     this.type = type;
     this.arrayCodec = codec.inDim();
+    this.arrayArrayCodec = arrayCodec.inDim();
   }
 
   @Provide
@@ -30,6 +32,11 @@ abstract class CodecTestBase<A> {
   @Provide
   Arbitrary<List<A>> arrayValues() {
     return net.jqwik.api.Arbitraries.randomValue(arrayCodec::random);
+  }
+
+  @Provide
+  Arbitrary<List<List<A>>> arrayArrayValues() {
+    return net.jqwik.api.Arbitraries.randomValue(arrayArrayCodec::random);
   }
 
   @Property(tries = 100)
@@ -45,6 +52,41 @@ abstract class CodecTestBase<A> {
     codec.write(sb, value);
     String encoded = sb.toString();
     A decoded = codec.parse(encoded, 0).value;
+    assertEquals(value, decoded);
+  }
+
+  @Property(tries = 100)
+  void decodesArrayEncodedInBinary(@ForAll("arrayValues") List<A> value) throws Exception {
+    byte[] encoded = arrayCodec.encodeInBinaryAsByteArray(value);
+    List<A> decoded = arrayCodec.decodeInBinary(ByteBuffer.wrap(encoded), encoded.length);
+    assertEquals(value, decoded);
+  }
+
+  @Property(tries = 100)
+  void decodesArrayEncodedInText(@ForAll("arrayValues") List<A> value) throws Exception {
+    StringBuilder sb = new StringBuilder();
+    arrayCodec.write(sb, value);
+    String encoded = sb.toString();
+    List<A> decoded = arrayCodec.parse(encoded, 0).value;
+    assertEquals(value, decoded);
+  }
+
+  @Property(tries = 100)
+  void decodesArrayArrayEncodedInBinary(@ForAll("arrayArrayValues") List<List<A>> value)
+      throws Exception {
+    byte[] encoded = arrayArrayCodec.encodeInBinaryAsByteArray(value);
+    List<List<A>> decoded =
+        arrayArrayCodec.decodeInBinary(ByteBuffer.wrap(encoded), encoded.length);
+    assertEquals(value, decoded);
+  }
+
+  @Property(tries = 100)
+  void decodesArrayArrayEncodedInText(@ForAll("arrayArrayValues") List<List<A>> value)
+      throws Exception {
+    StringBuilder sb = new StringBuilder();
+    arrayArrayCodec.write(sb, value);
+    String encoded = sb.toString();
+    List<List<A>> decoded = arrayArrayCodec.parse(encoded, 0).value;
     assertEquals(value, decoded);
   }
 }

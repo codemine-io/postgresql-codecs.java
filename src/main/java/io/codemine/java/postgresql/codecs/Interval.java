7b913a52e18d70d0b174bdc3f2045bcd2771791b
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-04-28 01:12:54 +0300

    Add conversions with Duration

--- src/main/java/io/codemine/java/postgresql/codecs/Interval.java
+++ src/main/java/io/codemine/java/postgresql/codecs/Interval.java
@@ -10,6 +10,41 @@ package io.codemine.java.postgresql.codecs;
  */
 public record Interval(long micros, int days, int months) {
 
+  /**
+   * Creates an {@code Interval} from a {@link java.time.Duration}. The duration is converted to an
+   * interval by decomposing it into days and microseconds, and then further decomposing the days
+   * into months (assuming 30 days per month). Note that this conversion is lossy and may not
+   * accurately represent the original duration, especially for durations that span multiple months
+   * or years, due to the fixed assumption of 30 days per month.
+   *
+   * <p>It also loses the nanosecond precision because the interval type only supports microseconds.
+   */
+  public static Interval of(java.time.Duration duration) {
+    long seconds = duration.getSeconds();
+    int nanos = duration.getNano();
+    long days = seconds / 86400L;
+    seconds %= 86400L;
+    long months = days / 30L;
+    days %= 30L;
+    long micros = seconds * 1_000_000L + nanos / 1000;
+    return new Interval(micros, (int) days, (int) months);
+  }
+
+  /**
+   * Converts this {@code Interval} to a {@link java.time.Duration}. The conversion is done by
+   * calculating the total number of seconds represented by the interval (including the time, day,
+   * and month components) and then creating a Duration from that total. Note that this conversion
+   * is lossy and may not accurately represent the original interval, especially for intervals that
+   * include month or day components, due to the fixed assumption of 30 days per month and the loss
+   * of nanosecond precision.
+   */
+  public java.time.Duration toDuration() {
+    long seconds = micros / 1_000_000L + (days + months * 30L) * 86400L;
+    int nanoAdjustment = (int) (micros % 1_000_000L) * 1000;
+
+    return java.time.Duration.ofSeconds(seconds, nanoAdjustment);
+  }
+
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
